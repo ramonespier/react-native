0fd5b15cb4a79f0ed21383a9a15f4335f4b4a08f
ramonespier <ramon.coelhomelo@outlook.com>
2025-08-08 07:57:26 -0300

    expo com typescript

--- App.tsx
+++ App.tsx
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.firstBlock}>
+        <Text style={styles.title}>M. Shadows</Text>
+      </View>
+
+      <View style={styles.secondBlock}>
+        <Text style={styles.title}>Synyster Gates</Text>
+      </View>
+
+      <View style={styles.thirdBlock}>
+        <Text style={styles.title}>Zacky Vengeance</Text>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'aliceblue',
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  
+  title: {
+    padding: 15,
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 800,
+    fontStyle: 'italic',
+  },
+
+  firstBlock: {
+    flex: 2,
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  
+  secondBlock: {
+    flex: 1,
+    backgroundColor: '#8B0000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  thirdBlock: {
+    flex: 4,
+    backgroundColor: '#4B4B4B',
+    alignItems: 'center',
+    justifyContent: 'center', 
+  },
+
+})
